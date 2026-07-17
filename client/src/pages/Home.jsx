7d05cdff760a9b1bac013f89813e6904e5c16581
OmethG <ometh.20240437@iit.ac.lk>
2026-07-17 10:32:44 +0530

    Fix logo import

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/logo.jpeg";
+import logo from "../assets/Logo.jpeg";
 
 export default function Home() {
   const navigate = useNavigate();
